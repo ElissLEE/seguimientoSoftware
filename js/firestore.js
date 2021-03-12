5a4e245013286c868e99303ab02cb307767d20ac
Melissa <meliortizmahone@gmail.com>
2021-03-12 16:05:15 -0500

    conexión con la base de datos

---
+++ js/firestore.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDbKs8y4TA7GURQ7ZbdM9aH1fzpAcNDYzg",
+    authDomain: "seguimiento-software-22ca8.firebaseapp.com",
+    projectId: "seguimiento-software-22ca8",
+    storageBucket: "seguimiento-software-22ca8.appspot.com",
+    messagingSenderId: "58247274030",
+    appId: "1:58247274030:web:ac7f7302551417796cc9bf"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
